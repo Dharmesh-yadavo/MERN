04960eff30aa8f2b676e2ce4dfc05f96f6219b85
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-11-22 10:59:05 +0530

    created client initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="text-3xl font-bold underline text-center">Hello everyone</p>
+    </>
+  );
+}
+
+export default App;
