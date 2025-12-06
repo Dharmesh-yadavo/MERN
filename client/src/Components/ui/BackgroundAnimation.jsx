597fe2508c44b0efd643cb0964451eae503108ed
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-12-06 13:12:46 +0530

    ui updated

--- client/src/Components/ui/BackgroundAnimation.jsx
+++ client/src/Components/ui/BackgroundAnimation.jsx
@@ -2,13 +2,13 @@ export const BackgroundAnimation = () => {
   return (
     <div className="absolute inset-0 z-10 overflow-hidden pointer-events-none">
       {/* Pink Blob */}
-      <div className="blob absolute top-[-100px] left-[1000px] w-[600px] h-[600px] bg-pink-600 animate-spinGradient"></div>
+      <div className="blob absolute top-[-200px] right-[150px] w-[500px] h-[400px] bg-pink-400 "></div>
 
       {/* Teal Blob */}
-      <div className="blob absolute top-1/2 left-1/2 w-[500px] h-[500px] bg-teal-600 animate-spinGradient "></div>
+      <div className="blob absolute bottom-[-100px] right-[300px] w-[450px] h-[450px] bg-orange-500 animation-delay-4000  "></div>
 
       {/* Orange Blob */}
-      <div className="blob absolute right-[-100px] bottom-[-100px] w-[500px] h-[500px] bg-orange-600 animate-spinGradient "></div>
+      <div className="blob absolute right-[-150px] bottom-[200px] w-[300px] h-[300px] bg-teal-500 animation-delay-2000 "></div>
     </div>
   );
 };
